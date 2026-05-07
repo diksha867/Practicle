3a06820f1cb0842e0be607d7a78fe1eb233f04d3
Diksha Sharma <dikshash867@gmail.com>
2026-05-07 10:19:18 +0530

    props

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useEffectEvent, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -6,6 +6,10 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+
+
+  }
 
   return (
     <>
